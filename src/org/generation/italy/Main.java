5cd79c3b26670e15a33e337cc6e1a7bdf9970fe8
claudiolore <claudio.lore001@gmail.com>
2023-11-22 12:26:31 +0100

    prova

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -22,7 +22,7 @@ public class Main {
 		m2.colore="Verde";
 		m2.pollici=24;
 		m2.accendi();
-		m2.riduciLuminosità();
+		m2.riduciLuminosita();
 		
 		
 		System.out.println("Inserisci i dati del monitor");
@@ -34,26 +34,12 @@ public class Main {
 		
 		//operazioni del monitor
 		do {
-			System.out.println("\n\n\n\n\n\n\n\n\nQuesto è il monitor m1:");
-			System.out.println("Colore: "+m1.colore);
-			System.out.println("Pollici: "+m1.pollici);
-			if (m1.stato==true)
-				System.out.println("Stato: acceso");
-			else
-				System.out.println("Stato: spento");
-			System.out.println("Luminosità: "+m1.luminosità);
+			System.out.println("Monitor 1: ");
+			m1.mostraDati();
+			System.out.println("Monitor 2: ");
+			m2.mostraDati();
 			
-			System.out.println("\n\nQuesto è il monitor m2:");
-			System.out.println("Colore: "+m2.colore);
-			System.out.println("Pollici: "+m2.pollici);
-			if (m2.stato==true)
-				System.out.println("Stato: acceso");
-			else
-				System.out.println("Stato: spento");
-			System.out.println("Luminosità: "+m2.luminosità);
-			
-			
-			System.out.println("Cosa vuoi fare con il monitor (accendi/spegni/aumenta luminosità/diminuisci luminosità/esci)?");
+			System.out.println("Cosa vuoi fare con il monitor (accendi/spegni/aumenta luminositï¿½/diminuisci luminositï¿½/esci)?");
 			operazione=sc.nextLine();
 			switch(operazione) {
 			case "accendi":
@@ -62,11 +48,11 @@ public class Main {
 			case "spegni":
 				m1.spegni();
 				break;
-			case "aumenta luminosità":
-				m1.aumentaLuminosità();
+			case "aumenta luminositï¿½":
+				m1.aumentaLuminosita();
 				break;
-			case "diminuisci luminosità":
-				m1.riduciLuminosità();
+			case "diminuisci luminositï¿½":
+				m1.riduciLuminosita();
 				break;
 			}
 			
--- src/org/generation/italy/Monitor.java
+++ src/org/generation/italy/Monitor.java
@@ -4,11 +4,11 @@ public class Monitor {
 	//variabili del monitor (attributi)
 	String colore;
 	int pollici;
-	int luminosità=50;
+	int luminosita=50;
 	boolean stato=false; 		//false: spento, true: acceso
 	
 	
-	//funzionalità (metodi)
+	//funzionalitï¿½ (metodi)
  	public void accendi() {
  		stato=true;
  	}
@@ -18,16 +18,33 @@ public class Monitor {
  		stato=false;
  	}
 	
- 	public void aumentaLuminosità()
+ 	public void aumentaLuminosita()
  	{
- 		if (luminosità<100)
- 			luminosità+=10;
+ 		if (luminosita<100)
+ 			luminosita+=10;
  	}
  	
- 	public void riduciLuminosità()
+ 	public void riduciLuminosita()
  	{
- 		if (luminosità>1)
- 			luminosità-=1;
+ 		if (luminosita>1)
+ 			luminosita-=1;
  	}
  	
+ 	public void mostraDati()
+ 	{
+ 		System.out.println("\n\n\n\n\n\n\n\n\nQuesto è il monitor m1:");
+		System.out.println("Colore: "+colore);
+		System.out.println("Pollici: "+pollici);
+		if (stato==true)
+			System.out.println("Stato: acceso");
+		else
+			System.out.println("Stato: spento");
+		System.out.println("Luminosita: ");
+		for(int i=0;i<luminosita/10;i++)
+			System.out.println("*");
+		System.out.println();
+ 	}
+
+
+
 }
